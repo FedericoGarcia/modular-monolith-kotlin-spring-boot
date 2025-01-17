8b2588768c6bff950393feae2538dcb3a2d71972
Federico García <garciafedericoagustin@gmail.com>
2025-01-17 10:50:15 -0300

    Refactor application test to validate instance creation instead of greeting

--- src/test/kotlin/org/example/ApplicationTest.kt
+++ src/test/kotlin/org/example/ApplicationTest.kt
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.Assertions.assertNotNull
 
 class ApplicationTest {
     @Test
-    fun appHasAGreeting() {
-        val classUnderTest = Application()
-        assertNotNull(classUnderTest.greeting, "app should have a greeting")
+    fun test() {
+        val app = Application()
+        assertNotNull(app)
     }
 }
