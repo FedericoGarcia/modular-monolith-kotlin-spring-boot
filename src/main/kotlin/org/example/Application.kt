af119db8138dea86522b8bb6f0e066c2d42bb65b
Federico García <garciafedericoagustin@gmail.com>
2025-01-17 19:25:15 -0300

    Remove unnecessary braces from Application class definition

--- src/main/kotlin/org/example/Application.kt
+++ src/main/kotlin/org/example/Application.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-open class Application {}
+open class Application
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
