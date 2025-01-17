552bd4de82e328e3a9d51fc2e79bbecc8abc43a0
Federico García <garciafedericoagustin@gmail.com>
2025-01-17 18:56:01 -0300

    Enhance application tests to validate instance creation and add HelloWorldController tests

--- src/test/kotlin/org/example/ApplicationTest.kt
+++ src/test/kotlin/org/example/ApplicationTest.kt
@@ -5,8 +5,23 @@ import org.junit.jupiter.api.Assertions.assertNotNull
 
 class ApplicationTest {
     @Test
-    fun test() {
+    fun testApplicationInstance() {
         val app = Application()
         assertNotNull(app)
+    }
+}
+
+class HelloWorldControllerTest {
+    @Test
+    fun testHelloWorldControllerInstance() {
+        val controller = HelloWorldController()
+        assertNotNull(controller)
+    }
+
+    @Test
+    fun testHelloWorld() {
+        val controller = HelloWorldController()
+        val response = controller.helloWorld()
+        assert(response == "Hello World!")
     }
 }
